eae2b7dada352120f7804073dee1428fa2351ca6
Daniel Braga <dbragabarbosa@gmail.com>
2022-04-19 19:50:08 -0300

    Aula 19:04

---
+++ aula 19:04/complexo.cpp
@@ -0,0 +1,15 @@
+// AQUI EU COLOCO TODOS OS DETALHES DE IMPLEMENTAÇÃO 
+
+#include "complexo.h"  
+
+Complexo conjugado()   //ESSA FUNÇÃO NÃO RECEBE PARÂMETRO, POIS ELA JÁ É CHAMADA POR ALGUM NÚMERO COMPLEXO ESPECIFICO 
+{
+    Complexo y;
+
+    y.real = real;
+    y.imag = -imag;
+
+    return y;    
+}
+
+
---
+++ aula 19:04/complexo.h
@@ -0,0 +1,10 @@
+// AQUI SÓ COLOCO A DEFINIÇÃO 
+
+struct Complexo
+{
+    double real;   // ATRIBUTOS DA STRUCT
+    double imag;
+    
+    Complexo conjugado()   //ESSA FUNÇÃO NÃO RECEBE PARÂMETRO, POIS ELA JÁ É CHAMADA POR ALGUM NÚMERO COMPLEXO ESPECIFICO 
+    
+};
