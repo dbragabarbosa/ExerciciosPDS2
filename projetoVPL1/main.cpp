987cb2db7074ea41426c9b5b38cb55b9ab71f7d4
Daniel Braga <dbragabarbosa@gmail.com>
2022-05-20 01:09:45 -0300

    VPLs

--- projetoVPL1/main.cpp
+++ projetoVPL1/main.cpp
@@ -1,6 +1,7 @@
 // O objetivo desse problema é iniciar a familiarização do aluno com o ambiente VPL e a linguagem C++.
 
-// Escreva um programa que simplemente apresente como saída o texto "Hello World". Lembre-se de não utilizar o comando printf, mas sim o comando cout (próprio de C++).
+// Escreva um programa que simplemente apresente como saída o texto "Hello World". Lembre-se de não utilizar o comando printf, 
+// mas sim o comando cout (próprio de C++).
 
 #include <iostream>
 
